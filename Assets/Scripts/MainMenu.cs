66aada3506d69c087c8fdb22e08d4bfa6e7bc1d5
cVLADIMIRc <taraev2004@mail.ru>
2023-12-24 21:09:23 +0700

    Создано меню с музыкой, игрок, враг

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    private AudioSource audioSource;
+	public void Start()
+	{
+        audioSource = GetComponent<AudioSource>();
+        audioSource.Play();
+	}
+	public void Play(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
