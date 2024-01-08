ff2f5eea9f19b536e16468b0a2e8d3db3e69458f
cVLADIMIRc <taraev2004@mail.ru>
2024-01-09 05:53:51 +0700

    Добавлен джойстик для ходбьбы и прыжка

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,58 +8,53 @@ using UnityEngine.Scripting.APIUpdating;
 
 public class Player : MonoBehaviour
 {
-    public float health;
-    public float speed;
-    public float jumpForce;
-    public float damage;
+	public float health;
+	public float speed;
+	public float jumpForce;
+	public float damage;
 
-    public bool isGrounded;
-    private Rigidbody2D rigidbody2D;
+	public bool isGrounded;
+	private Rigidbody2D rigidbody2D;
+	public Joystick joystick;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        rigidbody2D = GetComponent<Rigidbody2D>();
-    }
+	void Start()
+	{
+		rigidbody2D = GetComponent<Rigidbody2D>();
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-		if (Input.GetKeyDown(KeyCode.Space))
+	void Update()
+	{
+		if(joystick.Vertical > 0.5)
 		{
 			Jump();
 		}
 		die();
 	}
-	private void FixedUpdate()
+
+	void FixedUpdate()
 	{
-		Vector2 position = transform.position;
-
-        position.x += Input.GetAxis("Horizontal") * speed;
-        //position.y += Input.GetAxis("Vertical");
-
-        transform.position = position;  
+		rigidbody2D.velocity = new Vector2(joystick.Horizontal * speed, rigidbody2D.velocity.y);
 	}
-    public void Jump()
-    {
-        if (isGrounded) 
-        {
-            isGrounded = false;
-            rigidbody2D.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
-        }
-    }
+	public void Jump()
+	{
+		if (isGrounded && joystick.Vertical >= 0.5f)
+		{
+			isGrounded = false;
+			rigidbody2D.velocity = Vector2.up * jumpForce;
+		}
+	}
 	public void OnCollisionEnter2D(Collision2D collision)
 	{
-		if(collision.gameObject.tag == "Ground")
-        {
-            isGrounded = true;
-        }
+		if (collision.gameObject.tag == "Ground")
+		{
+			isGrounded = true;
+		}
 	}
-    public void die()
-    {
-        if(health <= 0)
-        {
-            Destroy(gameObject);
-        }
-    }
+	public void die()
+	{
+		if (health <= 0)
+		{
+			Destroy(gameObject);
+		}
+	}
 }
